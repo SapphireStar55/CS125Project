d69d627e8317508eea1b452bd9f1754bcd872e9a
4-Glory <122074354+4-Glory@users.noreply.github.com>
2025-03-13 11:40:24 -0700

    Update mining.c

--- mining.c
+++ mining.c
@@ -9,37 +9,36 @@ int mining(int location) {
 
 	switch location(
 
-	    //Bridge
-        case 1:
-    		    //gold yield
-    		    haul[0]=rand(time(NULL))%25+16;
-    
-    		    //platinum yield
-    		    haul[1]=rand(time(NULL))%11;
-    
-    		    //tritium yield
-    		    haul[2]=0;
-    
-    		    break;
-    
-        //St. Giles
-    	case 2:
-    			    //gold yield
-    			    haul[0]=0
-    
-    			            //platinum yield
-    			            haul[1]=rand(time(NULL))%11;
-    
-    			    //tritium yield
-    			    haul[2]=rand(time(NULL))%30+16;;
-    
-    			    break;
-    
-    			    //error
-        default:
-    				    printf("\033[0;31mYou can't mine in this location.\033[0m\n");
-
-    )
+	    	//Bridge
+	        case 1:
+		    //gold yield
+		    haul[0]=rand(time(NULL))%25+16;
+	
+		    //platinum yield
+		    haul[1]=rand(time(NULL))%11;
+	
+		    //tritium yield
+		    haul[2]=0;
+	
+		    break;
+	    
+	        //St. Giles
+		case 2:
+		    //gold yield
+		    haul[0]=0
+	    
+		    //platinum yield
+		    haul[1]=rand(time(NULL))%11;
+	    
+		    //tritium yield
+		    haul[2]=rand(time(NULL))%30+16;;
+	    
+		    break;
+	    
+	    	//error
+	        default:
+		    printf("\033[0;31mYou can't mine in this location.\033[0m\n");
+    	)
 
 	return 0;
 }
