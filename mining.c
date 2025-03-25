d8ade12a07abb81953ab16e457db91395f35c827
4-Glory <122074354+4-Glory@users.noreply.github.com>
2025-03-25 11:05:08 -0700

    Update mining.c

--- mining.c
+++ mining.c
@@ -5,41 +5,50 @@
 
 int mining(int location) {
 
-	int haul[3];
+	int haul[3]= {0,0,0};
+	char mine = 'y';
 
-	switch location(
+	while(mine == 'y') {
+		switch location(
 
-	    	//Bridge
-	        case 1:
-		    //gold yield
-		    haul[0]=rand(time(NULL))%25+16;
-	
-		    //platinum yield
-		    haul[1]=rand(time(NULL))%11;
-	
-		    //tritium yield
-		    haul[2]=0;
-	
-		    break;
-	    
-	        //St. Giles
-		case 2:
-		    //gold yield
-		    haul[0]=0
-	    
-		    //platinum yield
-		    haul[1]=rand(time(NULL))%11;
-	    
-		    //tritium yield
-		    haul[2]=rand(time(NULL))%30+16;;
-	    
-		    break;
-	    
-	    	//error
-	        default:
-		    printf("\033[0;31mYou can't mine in this location.\033[0m\n");
-    	)
+	    //Bridge
+	    case 1:
+			    printf("You begin mining...\n");
 
-	return 0;
+			    //gold yield
+			    haul[0]+=(rand(time(NULL))%25+16);
+
+			    //platinum yield
+			    haul[1]+=(rand(time(NULL))%11);
+
+			    printf("+%dgold\n+%dplatinum\n\n",haul[0],haul[1]);
+
+			    break;
+
+	    //St. Giles
+	    case 2:
+			    printf("You begin mining...\n");
+
+			    //tritium yield
+			    haul[2]+=(rand(time(NULL))%30+16);
+
+			    printf("+%dtritium\n\n",haul[2]);
+
+			    break;
+
+	    //error
+	    default:
+			    printf("\033[0;31mYou can't mine in this location.\033[0m\n");
+			    break;
+			)
+
+				//continue?
+				while(mine!='y')&&(mine!='n') {
+					printf("\nContinue mining?(y/n) ");
+					scanf("%c",&mine);
+				}
+
+	}
+
+	return haul;
 }
-
