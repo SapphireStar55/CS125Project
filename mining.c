474ac95b29fea9e3da49b5421c98d8d9899df4ed
4-Glory <122074354+4-Glory@users.noreply.github.com>
2025-03-25 11:40:05 -0700

    Update mining.c

--- mining.c
+++ mining.c
@@ -3,23 +3,25 @@
 #include <time.h>
 #include "inventory.h"
 
-int mining(int location) {
+int haul[3]= {0,0,0};
 
-	int haul[3]= {0,0,0};
+int* mining(int location) {
+
+	
 	char mine = 'y';
 
 	while(mine == 'y') {
-		switch location(
+		switch (location){
 
 	    //Bridge
 	    case 1:
 			    printf("You begin mining...\n");
 
 			    //gold yield
-			    haul[0]+=(rand(time(NULL))%25+16);
+			    haul[0]+=(rand()%25+16);
 
 			    //platinum yield
-			    haul[1]+=(rand(time(NULL))%11);
+			    haul[1]+=(rand()%11);
 
 			    printf("+%dgold\n+%dplatinum\n\n",haul[0],haul[1]);
 
@@ -30,7 +32,7 @@ int mining(int location) {
 			    printf("You begin mining...\n");
 
 			    //tritium yield
-			    haul[2]+=(rand(time(NULL))%30+16);
+			    haul[2]+=(rand()%30+16);
 
 			    printf("+%dtritium\n\n",haul[2]);
 
@@ -40,12 +42,14 @@ int mining(int location) {
 	    default:
 			    printf("\033[0;31mYou can't mine in this location.\033[0m\n");
 			    break;
-			)
+		}
 
 				//continue?
-				while(mine!='y')&&(mine!='n') {
+				printf("\nContinue mining?(y/n) ");
+					scanf(" %c",&mine);
+				while((mine!='y')&&(mine!='n')) {
 					printf("\nContinue mining?(y/n) ");
-					scanf("%c",&mine);
+					scanf(" %c",&mine);
 				}
 
 	}
