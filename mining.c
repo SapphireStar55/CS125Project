008699a1bae58b6debbacef557161bc32edb749e
4-Glory <122074354+4-Glory@users.noreply.github.com>
2025-03-27 09:30:44 -0700

    Update mining.c

--- mining.c
+++ mining.c
@@ -10,7 +10,7 @@ Version History: updated 03/25/25
 #include <time.h>
 #include "inventory.h"
 
-void mining(int location,Inventory* cargoHoldPointer) {
+void mining(int location, int* goldPointer, int* platinumPointer, int* tritiumPointer) {
 
 	int haul[3]= {0,0,0};
 	char mine = 'y';
@@ -23,13 +23,18 @@ void mining(int location,Inventory* cargoHoldPointer) {
 			    printf("You begin mining...\n");
 
 			    //gold yield
-			    haul[0] += (rand()%25+16);
-
+		    	    int goldLoad = (rand()%25+16);
+			    haul[0] += goldLoad;
+			    printf("+%dgold\n",goldLoad);
+		    
 			    //platinum yield
-			    haul[1] += (rand()%11);
+			    int platinumLoad = (rand()%11);
+			    haul[1] += platinumLoad;
 
-			    printf("+%dgold\n+%dplatinum\n\n",haul[0],haul[1]);
-
+		    	    if(platinumLoad==0)
+				    printf("\n");
+		    	    else
+				    printf("+%dplatinum\n\n",platinumLoad);
 			    break;
 
 	    //St. Giles
@@ -59,7 +64,19 @@ void mining(int location,Inventory* cargoHoldPointer) {
 
 	}
 
-	*cargoHoldPointer.gold.quantity += haul[0];
-	*cargoHoldPointer.platinum.quantity += haul[1];
-	*cargoHoldPointer.tritium.quantity += haul[2];
+	*goldPointer += haul[0];
+	*platinumPointer += haul[1];
+	*tritiumPointer += haul[2];
+
+	switch(location){
+		case 1:
+			if(haul[1]==0)
+				printf("%dkg of gold added to cargo hold.\n\n",haul[0]);	
+			else
+				printf("%dkg of gold and %dkg of platinum added to cargo hold.\n\n",haul[0],haul[1]);
+			break;
+		case 2:
+			printf("%dkg of tritium added to cargo hold.\n\n",haul[2]);
+			break;
+	}
 }
