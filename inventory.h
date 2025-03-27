c01f0da098f38353748ae6be053af862b53dc1fe
4-Glory <122074354+4-Glory@users.noreply.github.com>
2025-03-27 09:42:44 -0700

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -1,3 +1,11 @@
+/*
+File Name: inventory.h
+Author: Wesley Catlett
+Purpose: Contains structure definitions for Ore and Inventory.
+Version History: Last updated on 03/27/25
+*/
+
+
 struct ore{
   char oreType[20];
   int quantity;
