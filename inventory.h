0f3613c0ee2d0ec9a214a4418bcca58f9a0d3c00
4-Glory <122074354+4-Glory@users.noreply.github.com>
2025-03-27 11:03:22 -0700

    Create inventory.h

---
+++ inventory.h
@@ -0,0 +1,20 @@
+/**************************************************************************
+File Name: inventory.h
+Author: Wesley Catlett
+Purpose: Contains the structure and type definitions for Ore and Inventory.
+Version History: Last updated on 03/27/25
+**************************************************************************/
+
+//Ore definition
+typedef struct ore{
+  char oreType[20];
+  int quantity;
+  char units[20];
+} Ore;
+
+//Inventory definition
+typedef struct inventory{
+    Ore gold;
+    Ore platinum;
+    Ore tritium;
+} Inventory;
